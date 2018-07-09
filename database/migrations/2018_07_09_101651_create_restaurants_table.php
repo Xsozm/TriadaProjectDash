8b2526d6d1f06f4b31406440ca1d18eb3293f24b
ibraamEmad <36426055+ibraamEmad@users.noreply.github.com>
2018-07-09 13:34:35 +0200

    migrations add

---
+++ database/migrations/2018_07_09_101651_create_restaurants_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRestaurantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->String('name');
+            $table->String('email');
+            $table->String('logo');         
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('restaurants');
+    }
+}
